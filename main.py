eaa155c119899861c0f438b72caf7402e5b918ab
Marcin Czapski <marcinczapski1988@gmail.com>
2025-02-21 20:57:51 +0100

    Dodanie kodu: wyświetljącego listę sklepów i produktów do zakupienia

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ Lista_zakupow = {
         "piekarnia": ["chleb", "bułka", "pączek"],
         "warzywniak":["marchew","seler","rukola"]
         }
+
+print("Lista zakupów")
+for sklepy in Lista_zakupow:
+    print(f"Idę do {sklepy.capitalize()} kupuję tu następujące rzeczy: {[produkty.capitalize() for produkty in Lista_zakupow[sklepy]]}")
