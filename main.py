016296b061a8bb3339d6592bf746d24e3e58d767
Marcin Czapski <marcinczapski1988@gmail.com>
2025-02-21 21:03:19 +0100

    Dodanie kodu: wyświetlającego sumę produktów w liście zakupów

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ Lista_zakupow = {
 print("Lista zakupów")
 for sklepy in Lista_zakupow:
     print(f"Idę do {sklepy.capitalize()} kupuję tu następujące rzeczy: {[produkty.capitalize() for produkty in Lista_zakupow[sklepy]]}")
+print(f"W sumie kupuję {sum(map(len, Lista_zakupow.values()))} produktów")
