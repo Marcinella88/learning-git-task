3337a2b87c7e3e302432c72776dbbb7aef38baef
Marcin Czapski <marcinczapski1988@gmail.com>
2025-02-23 18:52:26 +0100

    Poprawienie kodu o zadania 1/ moduł 3/ wersja 8

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-# Zadanie: 3.2. Śledzenie zmian w kodzie
+# Zadanie 1 moduł 3
 
 Lista_zakupow = {
         "piekarnia": ["chleb", "bułka", "pączek"],
-        "warzywniak":["marchew","seler","rukola"]
-        }
+        "warzywniak":["marchew","seler","rukola"]}
+
+ilosc_produktow = 0
 
 print("Lista zakupów")
 for sklepy in Lista_zakupow:
-    print(f"Idę do {sklepy.capitalize()} kupuję tu następujące rzeczy: {[produkty.capitalize() for produkty in Lista_zakupow[sklepy]]}")
-print(f"W sumie kupuję {sum(map(len, Lista_zakupow.values()))} produktów")
+    print(f"Idę do {sklepy.capitalize()} kupuję tu następujące rzeczy: {[produkty.capitalize() for produkty in Lista_zakupow[sklepy]]}") 
+    ilosc_produktow += len(Lista_zakupow[sklepy])
+print(f"W sumie kupuję {ilosc_produktow} produktów")
 
 print("Zmiana przez drugiego programistę")
 
