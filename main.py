1dfce373e21818869ab000677f31d665afac306a
Marcin Czapski <marcinczapski1988@gmail.com>
2025-02-23 18:40:27 +0100

    Dodanie przez branch:kolega_1 printa do pliku main

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ for sklepy in Lista_zakupow:
     print(f"Idę do {sklepy.capitalize()} kupuję tu następujące rzeczy: {[produkty.capitalize() for produkty in Lista_zakupow[sklepy]]}")
 print(f"W sumie kupuję {sum(map(len, Lista_zakupow.values()))} produktów")
 
-Print("Zmiana przez drugiego programistę")
+print("Zmiana przez drugiego programistę")
+
+print("Branch: kolega_1 chciałbym dopisać tego printa")
